42c5d0a53abc74cbf375c192fea743d79bd935f3
Miafargo <isuurii@gmail.com>
2023-08-22 19:20:56 +0900

    working on form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  cardForm = this.fb.group({
+    name:['',Validators.required],
+    cardNumber:['',Validators.required],
+    month:['',Validators.required],
+    year:['',Validators.required],
+    cvc:['',Validators.required]
+  })
+  constructor(private fb: FormBuilder){}
+
+  onSubmit = () =>{
+    console.log('submitted form', this.cardForm.value)
+  }
+  
   title = 'interactive-form';
+
 }
